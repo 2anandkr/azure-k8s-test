6807a823a258909e920860937506ae162d509b23
anand <2anandkr@gmail.com>
2021-05-18 10:34:53 +0000

    get back code to test

--- cluster.tf
+++ cluster.tf
@@ -1,45 +1,45 @@
 
-# resource "azurerm_kubernetes_cluster" "cluster" {
-#   name                = "${var.prefix}-aks"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   dns_prefix          = "${var.prefix}-aks"
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-aks"
 
-#   default_node_pool {
-#     name       = "default"
-#     node_count = var.cluster_vm_min_count
-#     vm_size    = var.cluster_vm_size
-#     type       = "VirtualMachineScaleSets"
-#     # Availability Zones across which the Node Pool should be spread
-#     # availability_zones  = ["1", "2"]
-#     enable_auto_scaling = true
-#     min_count           = var.cluster_vm_min_count
-#     max_count           = var.cluster_vm_max_count
+  default_node_pool {
+    name       = "default"
+    node_count = var.cluster_vm_min_count
+    vm_size    = var.cluster_vm_size
+    type       = "VirtualMachineScaleSets"
+    # Availability Zones across which the Node Pool should be spread
+    # availability_zones  = ["1", "2"]
+    enable_auto_scaling = true
+    min_count           = var.cluster_vm_min_count
+    max_count           = var.cluster_vm_max_count
 
-#     # Required for advanced networking
-#     vnet_subnet_id = azurerm_subnet.subnet_1.id
-#   }
+    # Required for advanced networking
+    vnet_subnet_id = azurerm_subnet.subnet_1.id
+  }
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   role_based_access_control {
-#     azure_active_directory {
-#       managed                = true
-#       admin_group_object_ids = var.cluster_azure_ad_groups
-#       tenant_id              = var.cluster_azure_ad_tenant_id
-#     }
-#     enabled = true
-#   }
+  role_based_access_control {
+    azure_active_directory {
+      managed                = true
+      admin_group_object_ids = var.cluster_azure_ad_groups
+      tenant_id              = var.cluster_azure_ad_tenant_id
+    }
+    enabled = true
+  }
 
-#   network_profile {
-#     network_plugin    = "azure"
-#     load_balancer_sku = "standard"
-#     network_policy    = "calico"
-#   }
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+    network_policy    = "calico"
+  }
 
-#   tags = {
-#     Environment = var.prefix
-#   }
-# }
+  tags = {
+    Environment = var.prefix
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 
-# output "kube_config" {
-#   value = azurerm_kubernetes_cluster.cluster.kube_config_raw
-#   sensitive = true
-# }
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.cluster.kube_config_raw
+  sensitive = true
+}
 
 output "public_ip_win" {
   value = azurerm_public_ip.public_ip_win.*.ip_address
